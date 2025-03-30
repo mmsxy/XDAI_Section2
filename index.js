5b2873c4dad935957f0ed0b754ba794b59b14cfa
mmsxy <ql2677@nyu.edu>
2025-03-30 11:40:24 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BusinessSimulationForm from "./components/BusinessSimulationForm";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BusinessSimulationForm />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
